1920c7cdb4a16465998573553b1ac239fad1d142
MarcusSu <marcusssu@gmail.com>
2016-03-09 11:27:55 -0800

    loading for rawdata

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function() {});
+var projects = [];
 
-  $(".project_item").mouseover(function() {
+function Project(opts) {
+  this.title = opts.title;
+  this.type = opts.type;
+  this.content = opts.content;
+}
 
-    $(this).parent().removeClass("unselected");
-    $(this).parent().siblings().addClass("unselected");
+Project.prototype.toHTML = function() {
+  var $newProject = $('.template').clone();
+  //console.log($newProject);
+  $newProject.removeClass('template');
 
-  });
-  $(".project_item").mouseout(function() {
-    $(this).parent().removeClass("unselected");
-    $(this).parent().siblings().removeClass("unselected");
-  });
+  $newProject.find('.title').html(this.title);
+  $newProject.find('.type').html(this.type);
+  $newProject.find('.content').html(this.content);
+
+  return $newProject;
+
+};
+rawData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
+
+projects.forEach(function(a) {
+  $('.portfolio').find('.grid').append(a.toHTML());
 });
