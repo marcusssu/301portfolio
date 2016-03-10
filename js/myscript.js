a7f41299cb2571f57e05f73b989198c5f0beba65
MarcusSu <marcusssu@gmail.com>
2016-03-09 20:59:14 -0800

    done filter

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,7 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  projectView.populateFilters();
+  projectView.filterHandler();
+});
 var projects = [];
 
 function Project(opts) {
@@ -12,6 +15,10 @@ Project.prototype.toHTML = function() {
   //console.log($newProject);
   $newProject.removeClass('template');
 
+  //insert a data-type into type tag
+  $newProject.find('.type').attr('data-type', this.type);
+
+  //update content to each tags
   $newProject.find('.title').html(this.title);
   $newProject.find('.type').html(this.type);
   $newProject.find('.content').html(this.content);
@@ -19,10 +26,61 @@ Project.prototype.toHTML = function() {
   return $newProject;
 
 };
+//push each object into projects
 rawData.forEach(function(ele) {
   projects.push(new Project(ele));
 });
 
+//append each project into grid tag.
 projects.forEach(function(a) {
   $('.portfolio').find('.grid').append(a.toHTML());
 });
+
+var projectView = {};
+
+projectView.populateFilters = function() {
+  $('.portfolio').find('.col-12').each(function() {
+    if (!$(this).hasClass('template')) {
+      //grab the types form data-type
+      var types = $(this).find('.type').attr('data-type');
+
+      //split them into an array
+      var typeArray = types.split("&");
+
+      //for each item, create a option tag.
+      typeArray.forEach(function(item, index) {
+        //remove space
+        item = item.trim();
+        //if the array is not empty and not exist in the options then make a new one
+        if (item !== '' && $('#category-type option[value="' + item + '"]').length === 0) {
+          var optionTag = '<option value="' + item + '">' + item + '</option>';
+          $('#category-type').append(optionTag);
+        }
+      });
+    }
+  });
+};
+
+
+// display the related project tag by searching optionValue and tagValue
+// for example search "css" in "css & html"
+projectView.filterHandler = function() {
+  $('#category-type').on('change', function() {
+    var optionValue = $(this).val();
+    if ($(this).val()) {
+      $('.portfolio .col-12').hide();
+      $('.portfolio').find('.col-12:not(:first)').each(function(){
+        var thisTag = $(this);
+        var tagValue = $(this).find('.type').attr('data-type');
+        console.log(tagValue);
+        if (tagValue.search(optionValue)!=-1){
+          thisTag.fadeIn();
+        };
+      });
+    } else {
+      $('portfolio .col-12').fadeIn();
+      $('.template').hide();
+    }
+    //  $('#category-type').val('');
+  });
+}
--- js/rawData.js
+++ js/rawData.js
@@ -1,29 +1,37 @@
 var rawData = [
     {
       title: 'Project Title',
-      type: 'html&css',
+      type: 'html & css',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },
     {
       title: 'Project Title',
-      type: 'html&css',
+      type: 'Web Design & Front End Development',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },
     {
       title: 'Project Title',
-      type: 'html&css',
+      type: 'Angular.js & MVP',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },{
       title: 'Project Title',
-      type: 'html&css',
+      type: 'node.js & mongoDB',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },{
       title: 'Project Title',
-      type: 'html&css',
+      type: 'graphic design',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },{
       title: 'Project Title',
-      type: 'html&css',
+      type: 'html',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
+    },{
+      title: 'Project Title',
+      type: 'html & angular.js',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
+    },{
+      title: 'Project Title',
+      type: 'html & mongoDB',
       content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis urna in nisl placerat ullamcorper. Sed vehicula fermentum tortor at eleifend. In tellus quam, egestas et libero in, mollis dignissim elit. Nulla a justo tempus, luctus tortor posuere, sodales diam. Sed tristique cursus dolor nec lobortis. Etiam feugiat tincidunt tempus. Nam consequat vel augue non gravida. Cras venenatis orci odio, a pulvinar velit tempor eget. Aenean eget pulvinar justo. Integer mattis aliquam enim, eu blandit justo hendrerit in. Morbi hendrerit id enim auctor congue. Suspendisse maximus tempus mi, a fringilla massa tristique quis. Pellentesque dolor est, molestie id ultricies id, congue vel orci. Nulla facilisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec aliquet ex ac pellentesque tempor.'
     },
   ];
