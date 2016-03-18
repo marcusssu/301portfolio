59831fef74c0e7635f7a111dbe09b9c6e81a8b00
mevans72 <michaleevans72@gmail.com>
2016-03-18 13:54:58 -0700

    Making some very minor recomendations, but at this first look, this looks amazing!!!

--- js/projectModel.js
+++ js/projectModel.js
@@ -11,11 +11,11 @@
     var template = Handlebars.compile(source);
     return template(this);
   };
-
+//REVIEW: Minor suggestion. This is missing a single semicolon on line# 18 and #82.
   Project.loadAll = function(rawData) {
     rawData.forEach(function(ele) {
       Project.all.push(new Project(ele));
-    })
+    });
   };
   //******************************//
   //code reivew class-9
@@ -29,12 +29,14 @@
         type: 'HEAD',
         url: '/js/rawdata.json',
         success: function(data, message, xhr) {
+//REVIEW: Minor suggestion here. If the console.log() messages are no longer being used for testing, or if your are moving to production you may want to remove these console.logs. On startup, I see these on line# 33, 34, and 36, but it looks like there may be a few others as well.
           console.log(message);
           console.log(xhr);
           var eTag = xhr.getResponseHeader('eTag');
           console.log(eTag);
           //if localStorage does not have eTag or the current eTag not equal to localStorage's eTag
           if (!localStorage.eTag || eTag !== localStorage.eTag) {
+//REVIEW: This is a very minor recomendation, but if you wanted, you could set the value of localStorage.eTag to the $.getJSON... call directly. Again, this is only a minor recomendation and is purely based on preference or semantecs.
             localStorage.eTag = eTag;
 
             //******************************//
@@ -43,6 +45,8 @@
 
             $.getJSON('js/rawData.json', function(rawData) {
               //push the json file into a js object
+//REVIEW: You might want to look to combine a couple of these lines below, where possible. For example, you might want to look at something similar to:
+//Project.loadAll(JSON.stringify(localStorage.rawData));
               Project.loadAll(rawData);
               //cache the data into the localStorage
               var cache = JSON.stringify(rawData);
@@ -79,6 +83,6 @@
         callback();
       });
     }
-  }
+  };
   module.Project = Project;
 })(window);
