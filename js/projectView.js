8c27d853a37e04cbc117f1a37c8b6aaf527e4de2
MarcusSu <marcusssu@gmail.com>
2016-03-22 16:40:48 -0700

    remove node_modules

--- js/projectView.js
+++ js/projectView.js
@@ -1,9 +1,9 @@
 (function(module) {
 
 
-var projectView = {};
-projectView.populateFilters = function() {
-  $('.portfolio').find('.col-12').each(function() {
+  var projectView = {};
+  projectView.populateFilters = function() {
+    $('.portfolio').find('.col-12').each(function() {
 
       //grab the types form data-type
       var types = $(this).find('.type').attr('data-type');
@@ -22,12 +22,12 @@ projectView.populateFilters = function() {
           $('#category-type').append(optionTag);
         }
       });
-  });
-};
+    });
+  };
 
-// display the related project tag by searching optionValue inside tagValue
-// for example search "css" in "css & html"
-projectView.filterHandler = function() {
+  // display the related project tag by searching optionValue inside tagValue
+  // for example search "css" in "css & html"
+  projectView.filterHandler = function() {
     $('#category-type').on('change', function() {
       var optionValue = $(this).val();
       if ($(this).val()) {
@@ -60,49 +60,33 @@ projectView.filterHandler = function() {
     $('.portfolio').find('.col-12 .content').each(function() {
       var content = $(this).text();
       var wordCount = 0;
-
-      //count 100 word and hide all the contents after it.
-      // for (var j = 0; j < content.length; j++) {
-      //   if (content[j] == " ") {
-      //     wordCount = wordCount + 1;
-      //     if (wordCount == 100) {
-      //       //insert the prewrite hide class
-      //       content = content.insert(j, "<span class='hide'>");
-      //       content = content.insert(content.length, "</span>");
-      //       content = content.insert(content.length, "<p class='more-button'> ...more</p>");
-      //       $(this).html(content).contents();
-      //     }
-      //   }
-      // }
-
-
     });
   }
-projectView.readMoreButton = function(){
-  $('.content').on('click', '.more-button', function() {
-    console.log('click');
-    var thisButton = $(this);
-    var hiddenText = $(this).prev('.hide');
-    if (hiddenText.is(":hidden")) {
-      hiddenText.show();
-      hiddenText.addClass('fadein-animation');
-      $(this).text(' collapse');
-      setTimeout(function() {
-        hiddenText.removeClass("fadein-animation");
-        console.log('removeclass')
-      }, 500);
-      //hide the contents
-    } else {
-      hiddenText.addClass('fadeout-animation');
-      setTimeout(function() {
-        hiddenText.hide();
-        hiddenText.removeClass('fadeout-animation');
-        console.log(thisButton);
-        thisButton.text(' ...more');
-      }, 400);
-    }
-  });
-}
+  projectView.readMoreButton = function() {
+    $('.content').on('click', '.more-button', function() {
+      console.log('click');
+      var thisButton = $(this);
+      var hiddenText = $(this).prev('.hide');
+      if (hiddenText.is(":hidden")) {
+        hiddenText.show();
+        hiddenText.addClass('fadein-animation');
+        $(this).text(' collapse');
+        setTimeout(function() {
+          hiddenText.removeClass("fadein-animation");
+          console.log('removeclass')
+        }, 500);
+        //hide the contents
+      } else {
+        hiddenText.addClass('fadeout-animation');
+        setTimeout(function() {
+          hiddenText.hide();
+          hiddenText.removeClass('fadeout-animation');
+          console.log(thisButton);
+          thisButton.text(' ...more');
+        }, 400);
+      }
+    });
+  }
 
   projectView.navHandler = function() {
 
@@ -110,33 +94,15 @@ projectView.readMoreButton = function(){
       // e.preventDefault();
       $(this).parent().find('a').removeClass('activated');
       $(this).find('a').addClass('activated');
-      // if ($(this).data('content') == 'home') {
-      //   $('section').show();
-      // } else {
-      //
-      //   // console.log($(this).data('content'));
-      //   $('section').hide();
-      //   $('.' + $(this).data('content')).fadeIn();
-      //   if ($(this).data('content') == 'portfolio') {
-      //     console.log('test');
-      //     $('.filter').fadeIn();
-      //   }
-      // }
     });
   };
 
-  projectView.initIndexPage = function(){
+  projectView.initIndexPage = function() {
     projectView.navHandler();
-    // Project.fetchAll(projectView.initIndexPage);
-    Project.all.forEach(function(a){
-    $('.portfolio').find('.grid').append(a.toHTML());
-  });
-};
+    Project.all.forEach(function(a) {
+      $('.portfolio').find('.grid').append(a.toHTML());
+    });
+  };
 
-module.projectView = projectView;
-
-// $(document).ready(function() {
-//     console.log('init page');
-//     Project.fetchAll(projectView.initIndexPage);
-// });
+  module.projectView = projectView;
 })(window);
